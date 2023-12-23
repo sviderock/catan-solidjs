24517d2a2a2c7b6f2f6779525e46a48c3e515f2f
Myroslav Sviderok <myroslav.sviderok@gmail.com>
2023-12-23 22:51:34 +0200

    Add solid-devtools, fix esm/cjs issues on startup

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import solidPlugin from "vite-plugin-solid";
-// import devtools from 'solid-devtools/vite';
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
   plugins: [
@@ -9,12 +9,12 @@ export default defineConfig({
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     */
     devtools(),
-    solidPlugin(),
+    solidPlugin()
   ],
   server: {
-    port: 3000,
+    port: 3000
   },
   build: {
-    target: "esnext",
-  },
+    target: 'esnext'
+  }
 });
