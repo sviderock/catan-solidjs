d711e4275537e859b48b2bb5dd29fcc4ce676a00
Myroslav Sviderok <myroslav.sviderok@gmail.com>
2023-12-26 16:54:58 +0200

    Fix vite configs for railway build

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,24 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-import devtools from 'solid-devtools/vite';
+import { defineConfig, loadEnv } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import devtools from "solid-devtools/vite";
 
-export default defineConfig({
-  plugins: [
-    /* 
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-    devtools(),
-    solidPlugin()
-  ],
-  server: {
-    // eslint-disable-next-line no-undef
-    port: +process.env.PORT || 3000,
-    host: '0.0.0.0'
-  },
-  build: {
-    target: 'esnext'
-  }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [
+      /* 
+      Uncomment the following line to enable solid-devtools.
+      For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
+      */
+      mode === "development" ? devtools() : undefined,
+      solidPlugin()
+    ],
+    server: {
+      port: +env.PORT || 3000,
+      host: "0.0.0.0"
+    },
+    build: {
+      target: "esnext"
+    }
+  };
 });
