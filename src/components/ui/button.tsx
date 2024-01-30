84af5ed6201207104c88c23147ed1097c03f151e
Myroslav Sviderok <myroslav.sviderok@gmail.com>
2024-01-30 16:25:48 +0200

    Reduce default disabled Button opacity to 30%

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -7,7 +7,7 @@ import { cva } from "class-variance-authority";
 import { cn } from "@/utils";
 
 const buttonVariants = cva(
-  "ring-offset-background focus-visible:ring-ring inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 disabled:select-none",
+  "ring-offset-background focus-visible:ring-ring inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-30 disabled:select-none",
   {
     variants: {
       variant: {
