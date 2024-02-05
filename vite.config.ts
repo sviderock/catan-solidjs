e7f73dc7d5b2922b8b3213b6d67cc3004db88796
Myroslav Sviderok <myroslav.sviderok@gmail.com>
2024-02-05 17:31:39 +0200

    Remove devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
 import solidPlugin from "vite-plugin-solid";
-import devtools from "solid-devtools/vite";
 import checker from "vite-plugin-checker";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -8,7 +7,6 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
     plugins: [
-      mode === "development" ? devtools() : undefined,
       solidPlugin(),
       tsconfigPaths({ root: ".", projects: ["tsconfig.json"] }),
       checker({
